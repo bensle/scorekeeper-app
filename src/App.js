2527571e57df656d9e1436242db06766dd6af8c6
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-05-31 23:45:10 +0200

    Add player array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Playerdata } from './Playerdata';
 import Button from './Button';
 import Heading from './Heading';
 import Player from './Player';
@@ -8,9 +9,13 @@ function App() {
   return (
     <div className="App">
       <Heading />
-      <ul>
-        <Player text="John Doe" score={30} />
-        <Player text="Jane Doe" score={30} />
+      {/* eslint-disable */}
+      <ul className="Player__list" role="list">
+        {Playerdata.map((singlePlayer) => (
+          <Player key={singlePlayer.id} singlePlayer={singlePlayer} />
+        ))}
+        {/* <Player text="John Doe" score={30} />
+        <Player text="Jane Doe" score={30} /> */}
       </ul>
       <Button text="Reset scores" />
       <Button text="New game" />
--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,11 @@
 import './Player.css';
-export default function Player({ text, score }) {
+export default function Player({ singlePlayer }) {
   return (
     <li className="Player">
-      <span className="player__name">{text}</span> <button>-</button> {score}
-      <button>+</button>
+      <span className="Player__name">{singlePlayer.name}</span>
+      <button className="Player__button">-</button>
+      <span className="Player__score">{singlePlayer.score}</span>
+      <button className="Player__button">+</button>
     </li>
   );
 }
@@ -13,3 +15,5 @@ export default function Player({ text, score }) {
 //   <h2 className="playerscore">{score}</h2>
 //   <button className="subtractbutton">-</button>
 // </section>
+
+// {/* <span className="player__name">{text}</span> */}
